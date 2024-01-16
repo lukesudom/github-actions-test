16e71c3fed3248ac8afd1abff89e4d36069e5190
Luke Marshall <89007022+lukesudom@users.noreply.github.com>
2024-01-17 09:26:55 +1100

    Create script.py

---
+++ script.py
@@ -0,0 +1,15 @@
+import requests
+import os
+from json import dumps
+
+if  __name__ == "__main__":
+    TOKEN = os.getenv('API_KEY')
+    print("key ends in: ")
+    print(TOKEN[:10])
+    
+    url = '<https://api.thecatapi.com/v1/images/0XYvRd7oD'
+    headers = {
+        'x-api-key' : TOKEN
+    }
+    cat_info = requests.get(url, headers)
+    print(dumps(cat_info.json(), indent = 1))
